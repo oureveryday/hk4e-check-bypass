3b673370234f013e0f3f245fd37b0c49652cdd35
oureveryday <oureveryday@163.com>
2024-06-23 17:35:51 +0800

    Update dllmain.cpp

--- mhypbase/src/dllmain.cpp
+++ mhypbase/src/dllmain.cpp
@@ -1,27 +1,17 @@
 #include "pch.h"
 
-#include <iostream>
-
-DWORD WINAPI Thread(LPVOID lpParam)
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
-	std::cout << "[AntiCheatEMU] Thread started" << std::endl;
-	return 0;
+	return TRUE;
 }
 
 HRESULT __cdecl Initialize()
 {
-	std::cout << "[AntiCheatEMU] Initialize called..." << std::endl;
-	Sleep(500);
-	while(true)
-	{
-		Sleep(30000);
-	}
+	LoadLibraryA("mhypbase.dll");
+	OutputDebugStringW(L"[AntiCheatEMU] Initialize");
+	OutputDebugStringW(L"[AntiCheatEMU] TerminateThread");
+	TerminateThread(GetCurrentThread(),0);
+	return false;
 }
 
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
-{
-	if (fdwReason == DLL_PROCESS_ATTACH)
-		if (HANDLE hThread = CreateThread(NULL, 0, Thread, NULL, 0, NULL))
-			CloseHandle(hThread);
-	return TRUE;
-}
+
